4fa7f0a2426844cd230544a5cab27c072746b942
reznot <Reznot24mj.invincible@gmail.com>
2019-05-21 23:45:41 +0200

    beta version ready

--- LZ78.py
+++ LZ78.py
@@ -1,5 +1,6 @@
 def encode(text_to_encode):
     text = text_to_encode.replace(" ", "")
+    print("Rozpoczynam kodowanie LZ78")
     print(f"\nUsunięto spacje: {text}")
     print("Tworze pusty słownik\n")
     dicionary = {}
@@ -8,6 +9,7 @@ def encode(text_to_encode):
     last_word_index = 0
     code = []
     encoded = []
+    added_new = False
 
     for index in range(len(text)):
         tmp = tmp + text[index]
@@ -29,10 +31,13 @@ def encode(text_to_encode):
                 print(f"{k+1}: {v}")
             print("\n")
             tmp = ''
+            added_new = True
         else:
             last_word_index = dicionary[tmp]
-        # input("Aby przejść dalej nacisnij enter\n")
-    print("Zakodowano cały tekst!!!")
+        if added_new:
+            input("Aby kontynuować kodowanie wciśnij enter\n")
+            added_new = False
+    print("Zakodowano cały tekst!!!\n")
     return encoded
 
 def decode(encoded):
@@ -40,25 +45,34 @@ def decode(encoded):
     dict_index = 1
     tmp = ""
     decoded_text = ""
+    print("Rozpoczynam dekodowanie LZ78")
 
     for code in encoded:
+        print(f"Kod: {code}")
         splitted = code.split(":>")
         splitted[0] = int(splitted[0])
         if splitted[0] == 0:
+            print(f"Litera {splitted[1]} występuje po raz pierwszy i trafia do słownika\nZostaje dodana do zdekodowanego tekstu")
             dicionary[dict_index] = splitted[1]
             dict_index += 1
             decoded_text = decoded_text + splitted[1]
+            print(f"Zdekodowany tekst: {decoded_text}\n")
         else:
+            print(f"Pod indeksem {splitted[0]} w slowniku znajduje się \"{dicionary[splitted[0]]}\"")
+            print(f"Dodaje literkę \"{splitted[1]}\"")
             tmp = dicionary[splitted[0]] + splitted[1]
+            print(f"Dodaje do słownika \"{tmp}\"")
             dicionary[dict_index] = tmp
             dict_index += 1
             decoded_text = decoded_text + tmp
-    print(decoded_text)
+            print(f"Zdekodowany tekst: {decoded_text}\n")
+        input("Aby kontynuować dekodowanie wcisnij enter\n")
+    print("Tekst został odkodowany")
 
 text_to_encode = input("Podaj tekst który chcesz zakodować: ")
 encoded_text = encode(text_to_encode)
-
-input("Aby rozpocząć dekodowanie wciśnij enter")
+print("=======================================================\n")
+input("Aby rozpocząć dekodowanie wciśnij enter\n")
 decode(encoded_text)
 
 # a-bar-array-by-barrayar-bay
